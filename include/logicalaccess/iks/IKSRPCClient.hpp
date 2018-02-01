634ca1178e42a391a2b09a28fcafa12d1a2f6251
Kapp Arnaud <kapp.arno@gmail.com>
2018-02-01 18:50:04 +0100

    format

--- include/logicalaccess/iks/IKSRPCClient.hpp
+++ include/logicalaccess/iks/IKSRPCClient.hpp
@@ -8,31 +8,34 @@
 #include <grpc++/security/credentials.h>
 #include "logicalaccess/iks/packet/iks.grpc.pb.h"
 
-namespace logicalaccess {
-    namespace iks {
-        /**
-         * Wraps a RPC client to IKS.
-         */
-        class IKSRPCClient {
-        public:
-            IKSRPCClient(IslogKeyServer::IKSConfig config);
+namespace logicalaccess
+{
+namespace iks
+{
+/**
+ * Wraps a RPC client to IKS.
+ */
+class IKSRPCClient
+{
+  public:
+    IKSRPCClient(IslogKeyServer::IKSConfig config);
 
-            ByteVector gen_random(int size);
+    ByteVector gen_random(int size);
 
-            ByteVector aes_encrypt(const ByteVector &in, const std::string &key_name,
-                                   const ByteVector &iv);
+    ByteVector aes_encrypt(const ByteVector &in, const std::string &key_name,
+                           const ByteVector &iv);
 
-            ByteVector aes_decrypt(const ByteVector &in, const std::string &key_name,
-                                   const ByteVector &iv);
+    ByteVector aes_decrypt(const ByteVector &in, const std::string &key_name,
+                           const ByteVector &iv);
 
-            SMSG_DesfireAuth_Step1 desfire_auth_step1(CMSG_DesfireAuth_Step1 req);
-            SMSG_DesfireAuth_Step2 desfire_auth_step2(CMSG_DesfireAuth_Step2 req);
+    SMSG_DesfireAuth_Step1 desfire_auth_step1(CMSG_DesfireAuth_Step1 req);
+    SMSG_DesfireAuth_Step2 desfire_auth_step2(CMSG_DesfireAuth_Step2 req);
 
 
-        private:
-            IslogKeyServer::IKSConfig config_;
-            std::shared_ptr<::grpc::ChannelInterface> channel;
-            std::unique_ptr<IKSService::Stub> stub_;
-        };
-    }
+  private:
+    IslogKeyServer::IKSConfig config_;
+    std::shared_ptr<::grpc::ChannelInterface> channel;
+    std::unique_ptr<IKSService::Stub> stub_;
+};
+}
 }
